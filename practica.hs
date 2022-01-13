a7410c1379763e5df396ddd6358a927e6b335b6d
Adrian <adrianestevezg@gmail.com>
2022-01-13 20:44:08 +0100

    primera parte

--- practica.hs
+++ practica.hs
@@ -17,15 +17,61 @@ hash (x:xs)
 hash [] = 0
 
 --tipo de datos hash
-data Hash = Hash Int ([Char], [Char]) deriving (Show, Read)
+data Hash = Hash Int ([Char], [Char])
+
+instance Show Hash where
+  show (Hash _ (a,b)) = "("++a++" = "++b++")"
+
+--type Traduccion = ([Char], [Char])
+--type Hash = (Int, Traduccion)
+
+hashAll :: [[Char]] -> [Hash]
+hashAll (x:xs) = let a = takeWhile (/= ' ') x in let b = tail(dropWhile (/= ' ') x) in Hash (hash(a)) (a, b):hashAll xs
+hashAll  [] = []
+
+--devuelve true si el id del hash concuerda con el id introducido
+hashHasIdx :: Int -> Hash -> Bool
+hashHasIdx i (Hash idx _) = idx == i
+
+filterHash :: Int -> [Hash] -> [Hash]
+filterHash  idx xs = filter(hashHasIdx idx) xs
+
+
+--devuleve un array de hash en un string
+hashArrayToStr :: [Hash] -> String
+hashArrayToStr (x:xs) = show x ++  hashArrayToStrAux xs
+hashArrayToStr [] = ""
+
+hashArrayToStrAux :: [Hash] -> String
+hashArrayToStrAux (x:xs) = " , " ++ show x ++ hashArrayToStrAux xs
+hashArrayToStrAux [] = ""
+
 
 --cargar fichero
 --cargarDiccionario :: FilePath -> IO ()
---cargarDiccionario = do
---    cs <- readFile "datos.txt"
---    putStrLm cs
 
-muestraContenidoFichero :: FilePath -> IO ()
-muestraContenidoFichero f = do
-  cs <- readFile f
-  putStrLn cs
+
+cargarContenidoFichero :: IO [Hash]
+cargarContenidoFichero = do
+  fileContent <- readFile "datos.txt"
+  let fileLines = lines fileContent in let tablaHash = (hashAll(fileLines)) in return tablaHash--putStrLn ("[0]->"++hashArrayToStr(filterHash 0 tablaHash))
+  --putStrLn "[0]->"++hashHasIdx
+
+mostrarDiccionario :: IO()
+mostrarDiccionario = do
+  tablaHash <- cargarContenidoFichero
+  mostrarLineaDiccionario 0 tablaHash
+
+mostrarLineaDiccionario :: Int -> [Hash] -> IO()
+mostrarLineaDiccionario i tablaHash = do
+  if i > 9 || i < 0 then return ()
+  else do 
+    putStrLn ("["++show i++"]->"++hashArrayToStr(filterHash i tablaHash))
+    mostrarLineaDiccionario (i+1) tablaHash
+
+--Hash hash(takeWhile (/= ' ') fileLines !! 0) (takeWhile(/= ' ') fileLines !! 0, tail(dropWhile (/= ' ') fileLines !! 0))
+
+--findHashIdx :: [Hash] -> [Hash]
+
+
+
